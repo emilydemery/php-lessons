308f4456702a0e886559df103497b69a98fe4ff7
Emily Emery <emilydemery@gmail.com>
2011-08-01 18:33:03 -0400

    added name

--- sms-hello-monkey.php
+++ sms-hello-monkey.php
@@ -1,7 +1,18 @@
 <?php
-	header("content-type: text/xml");
-	echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+// make an associative array of senders we know, indexed by phone number
+$people = array(
+"+16465304568"=>"Emily",
+"+14158675310"=>"Boots",
+"+14158675311"=>"Virgil",
+);
+// if the sender is known, then greet them by name
+// otherwise, consider them just another monkey
+if(!$name = $people[$_REQUEST['From']])
+$name = "Monkey";
+// now greet the sender
+header("content-type: text/xml");
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 ?>
 <Response>
-	<Sms>Hello, Mobile Monkey</Sms>
+<Sms><?php echo $name ?>, thanks for the message!</Sms>
 </Response>
