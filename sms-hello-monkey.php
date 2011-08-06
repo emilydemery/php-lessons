7f8288ec725e4a7a7f032588f199e4fa27059bd6
Emily Emery <emilydemery@gmail.com>
2011-08-06 19:17:18 -0400

    update

--- sms-hello-monkey.php
+++ sms-hello-monkey.php
@@ -8,11 +8,11 @@ $people = array(
 // if the sender is known, then greet them by name
 // otherwise, consider them just another monkey
 if(!$name = $people[$_REQUEST['From']])
-$name = "Monkey";
+$name = "Fun Party Person";
 // now greet the sender
 header("content-type: text/xml");
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 ?>
 <Response>
-<Sms><?php echo $name ?>, thanks for the message!</Sms>
+<Sms><?php echo $name ?>, come to our party! 135 Conselyea Street 3A, on the Graham stop of the L. Call Emily to come up to the roof: 6465304568. See you soon!</Sms>
 </Response>
